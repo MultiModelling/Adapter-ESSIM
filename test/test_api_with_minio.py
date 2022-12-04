497864f7b7721e96d4944e2d62d81be1ff42ffed
Edwin Matthijssen <edwin.matthijssen@tno.nl>
2022-12-04 14:40:01 +0100

    Fix test

--- test/test_api_with_minio.py
+++ test/test_api_with_minio.py
@@ -42,9 +42,10 @@ post_body = {
             ]
         },
     },
-    # "input_esdl_file_path": "bedrijventerreinommoord/Scenario_1_II3050_Nationale_Sturing/Trial_1/MM_workflow_run_1/ESDL_add_price_profile_adapter/Hybrid HeatPump.esdl",
-    "input_esdl_file_path": "bedrijventerreinommoord/Scenario_1_II3050_Nationale_Sturing/Trial_1/MM_workflow_run_1/ESDL_add_price_profile_adapter/HHP_profile.esdl",
-    "output_file_path": "bedrijventerreinommoord/Scenario_1_II3050_Nationale_Sturing/Trial_1/MM_workflow_run_1/ESSIM_adapter/KPIs.json"
+    "base_path": "bedrijventerreinommoord/Scenario_1_II3050_Nationale_Sturing/Trial_1/MM_workflow_run_1/",
+    # "input_esdl_file_path": "ESDL_add_price_profile_adapter/Hybrid HeatPump.esdl",
+    "input_esdl_file_path": "ESDL_add_price_profile_adapter/HHP_profile.esdl",
+    "output_file_path": "ESSIM_adapter/KPIs.json"
 }
 
 res = requests.post(api_endpoint + '/model/initialize/' + model_run_id, json=post_body)
